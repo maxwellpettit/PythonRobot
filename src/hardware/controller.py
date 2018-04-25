f92c5bfe5d63b133ecef748abbb37174db8620c7
maxwellpettit <maxwell.pettit@gmail.com>
2018-04-25 19:17:46 -0400

    Add new controller functionality

--- src/hardware/controller.py
+++ src/hardware/controller.py
@@ -1,15 +1,81 @@
-import asyncio, evdev
+#!/usr/bin/python3
 
-xbox = evdev.InputDevice('/dev/input/event0')
+import asyncio, evdev, time
 
-print(xbox.capabilities(verbose=True))
+class Controller():
+    """
+    Buttons:
+    'BTN_A' - A
+    'BTN_B' - B
+    'BTN_X' - X
+    'BTN_Y' - Y
+    'BTN_TL' - Left Bumper
+    'BTN_TR' - Right Bumper
+    'BTN_SELECT' - Select 
+    'BTN_START' - Start
+    'BTN_MODE' - Xbox
+    'BTN_THUMBL' - Left Thumb Press 
+    'BTN_THUMBR' - Right Thumb Press
+    
+    Axes:
+    'ABS_X' - Left X - (-32768, 32767)
+    'ABS_Y' - Left Y - (-32768, 32767)
+    'ABS_RX' - Right X - (-32768, 32767)
+    'ABS_RY' - Right Y - (-32768, 32767)
+    'ABS_GAS' - Right Trigger - (0, 255)
+    'ABS_BRAKE' - Left Trigger - (0, 255)
+    'ABS_HAT0X' - D-Pad X - (-1, 1)
+    'ABS_HAT0Y' - D-Pad Y - (-1,1)
+    """
 
-async def print_events(device):
-    async for event in device.async_read_loop():
-        print(device.fn, evdev.categorize(event), sep=': ')
-        print(event)
+    MAX_JOYSTICK = 32767
 
-asyncio.ensure_future(print_events(xbox))
+    left = 0
+    right = 0
 
-loop = asyncio.get_event_loop()
-loop.run_forever()
+    def __init__(self, port=0):
+        self.xbox = evdev.InputDevice('/dev/input/event' + str(port))
+        print(self.xbox.capabilities(verbose=True))
+        asyncio.ensure_future(self.print_events(self.xbox))
+
+    async def print_events(self, device):
+        async for event in device.async_read_loop():
+            print(device.fn, evdev.categorize(event), sep=': ')
+            
+            if (event.type == evdev.ecodes.EV_KEY):
+                print("******************** Button Pressed ********************")
+                btnEvent = evdev.categorize(event)
+                if (btnEvent.keystate == evdev.KeyEvent.key_down):
+                    if (btnEvent.keycode == 'BTN_A'):
+                        print("******************** A Pressed ********************")
+            
+            elif (event.type == evdev.ecodes.EV_ABS):
+                print("******************** Joystick Event ********************")
+                if (event.code == 'ABS_Y'):
+                    self.left = event.value / self.MAX_JOYSTICK
+                elif (event.code == 'ABS_RY'):
+                    self.right = event.value / self.MAX_JOYSTICK
+
+    def start(self):
+        self.loop = asyncio.get_event_loop()
+        self.loop.run_forever()
+
+    def stop(self):
+        asyncio.gather(*asyncio.Task.all_tasks()).cancel()
+        self.loop.stop()
+        self.loop.close()
+
+
+if __name__ == '__main__':
+    xbox = Controller()
+    xbox.start()
+
+    time.sleep(10)
+
+    print("Left: " + str(xbox.left))
+    print("Right: " + str(xbox.right))
+
+    time.sleep(10)
+
+    print("Left: " + str(xbox.left))
+    print("Right: " + str(xbox.right))
